31d36a62e10f7f904dc32486a25ecd3895fa33aa
luizfcavalcantegomes <luizfcavalcantegomes@gmail.com>
2021-03-09 13:51:55 -0500

    Update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabeçalho da Página.")
+        self.assertRegex(result.data.decode(), "My english classes are the best ones in the world")
 
 
 if __name__ == "__main__":
