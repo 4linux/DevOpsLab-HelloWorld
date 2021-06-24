d8238c4cd91d02962baca54d9f89f3d5d26ae873
renanhermann <48216905+renanhermann@users.noreply.github.com>
2021-06-24 17:12:13 -0300

    upda
    update .py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "fenanda loves true")
+        self.assertRegex(result.data.decode(), "fenanda lovestrue")
 
 
 if __name__ == "__main__":
