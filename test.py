d672835e86df611f970080f228fc408232bdbd64
Caio Delgado <caiodelgado.new@gmail.com>
2021-08-02 16:55:47 +0200

    DevOps Lab 2021

---
+++ test.py
@@ -0,0 +1,28 @@
+from app import app
+import unittest
+
+class Test(unittest.TestCase):
+    
+    def setUp(self):
+        # cria uma instancia do unittest, precisa do nome "setUp"
+        self.app = app.test_client()
+
+    def test_requisicao(self):
+        # envia uma requisicao GET para a URL
+        result = self.app.get('/')
+
+        # compara o status da requisicao (precisa ser igual a 200)
+        self.assertEqual(result.status_code, 200) 
+
+    def test_conteudo(self):
+        # envia uma requisicao GET para a URL
+        result = self.app.get('/') 
+
+        # verifica o retorno do conteudo da pagina
+        self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabecalho da Pagina.")
+
+
+if __name__ == "__main__":
+    print ('INICIANDO OS TESTES')
+    print('----------------------------------------------------------------------')
+    unittest.main(verbosity=2)
