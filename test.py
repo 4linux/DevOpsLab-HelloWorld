ec8b4f280009fc25ef6574a3195688bc2d27c670
alexandremsg <alexandre.monteiro.info@gmail.com>
2021-10-14 06:03:09 -0400

    atulizando cabeçalho da pagina

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabecalho da Pagina.")
+        self.assertRegex(result.data.decode(), "Seja bem vindo ao meu portifolio DevOps =).")
 
 
 if __name__ == "__main__":
