6378bcc01d0465dc1c35c37d49e541b64fd78f9a
pretobart <pretobart@gmail.com>
2021-10-25 20:37:02 -0400

    Add pipeline README1

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabecalho da Pagina.")
+        self.assertRegex(result.data.decode(), "Minha pagina (DevOps)")
 
 
 if __name__ == "__main__":
