0a96f7bf43eb13c7b80f6ec163bec0410af72f64
pretobart <pretobart@gmail.com>
2021-10-26 13:43:57 -0400

    Teste Heroku 3.0

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Minha pagina (DevOps 2.0)")
+        self.assertRegex(result.data.decode(), "Minha pagina (DevOps 3.0)")
 
 
 if __name__ == "__main__":
